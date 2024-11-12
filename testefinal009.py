98ccd255186d06767233d4e5a44d3eb16f75b8be
Heitor Silva <heitorsilvanascimento@hotmail.com>
2024-11-12 14:30:19 -0300

    Add files via upload

---
+++ testefinal009.py
@@ -0,0 +1,5 @@
+#Considere a string s = 'prova de python'. A instrução len(s) retornaria qual valor?
+
+s = 'prova de python'
+comprimento = len(s)
+print('O comprimento do texto é:{}'.format(comprimento))
